cca110e8766cc31b802d6325e3aa56b7958bef1d
Steve Macurdy <steve@users.noreply.github.com>
2026-03-03 07:14:05 -0700

    fix: exclude legacy test from vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   test: {
     environment: 'node',
     globals: true,
+    exclude: ["tests/**", "node_modules/**"],
     setupFiles: ['./__tests__/setup.ts'],
   },
   resolve: {
